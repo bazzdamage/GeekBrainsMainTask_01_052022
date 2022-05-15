e93ea1cb193e8e26f613d41113a0b16186121b01
bazzdamage <bazzdamage@outlook.com>
2022-05-16 00:00:53 +0700

    Add ProcessedStringCount, MakeArray and PrintArray methods

--- Utility.cs
+++ Utility.cs
@@ -4,5 +4,36 @@ namespace GeekBrainsMainTask_01_052022
 {
     internal class Utility
     {
+        public static int ProcessedStringCount(string[] stringArray, int wordlength)
+        {
+            int stringCount = 0;
+            foreach (string word in stringArray)
+            {
+                if (word.Length >= wordlength) stringCount++;
+            }
+            return stringCount;
+        }
+        public static string[] MakeArray(string[] stringArray, int stringCount, int wordlength)
+        {
+            string[] resultArray = new string[stringCount];
+            int j = 0;
+            foreach (string word in stringArray)
+            {
+                if (word.Length >= wordlength)
+                {
+                    resultArray[j] = word;
+                    j++;
+                }
+            }
+            return resultArray;
+        }
+        public static void PrintArray(string[] stringArray, int stringCount, int wordlength)
+        {
+            Console.WriteLine($"Total count of strings, that contains {wordlength} or less symbols are {stringCount}...\n...And here they are");
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+        }
     }
 }
